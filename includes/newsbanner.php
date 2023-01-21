f8b84b87e0f4819e31905afd7aefd8c03742f25f
endedman <88163210+Endedman@users.noreply.github.com>
2023-01-21 20:39:09 +0300

    Delete includes directory

--- includes/newsbanner.php
+++
@@ -1,3 +0,0 @@
-<div class="redbanner">
-<?php echo $languagesel; ?>&nbsp;<a href='http://j2me.ml?lang=ru'>RU</a>&nbsp;<a href='http://j2me.ml?lang=en'>EN</a>&nbsp;<a href='http://j2me.ml?lang=es'>ES</a>&nbsp;<a href='http://j2me.ml?lang=pl'>PL</a>&nbsp;<a href='http://j2me.ml?lang=ce'>CENSORSHIP</a>
-</div>
